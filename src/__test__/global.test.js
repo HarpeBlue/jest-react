3ae7e8cc19dc41cf2b03aa6172eabbfa3c672e9e
HarpeBlue <hspe2507@gmail.com>
2021-01-24 23:01:27 -0500

    feat: numbers, strings and callbacks

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,5 +1,28 @@
 const text = "Hola mundo";
+const fruits = ['manzana', 'melon', 'banana'];
 
 test('Debe contener un texto', () => {
-  expect(text).toMatch(/mundo/)
+  expect(text).toMatch(/mundo/);
+});
+
+test('¿tenemos una banana?', () => {
+  expect(fruits).toContain('banana');
+});
+
+test('Mayor que', () => {
+  expect(10).toBeGreaterThan(9);
+});
+
+test('Es verdadero', () => {
+  expect(true).toBeTruthy();
+});
+
+const reverseString = (str, callback) => {
+  callback(str.split("").reverse().join(""))
+}
+
+test('Probar callback', () => {
+  reverseString('Hola', (str) => {
+    expect(str).toBe('aloH');
+  })
 });
